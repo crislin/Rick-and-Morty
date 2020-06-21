113d0a99107a5d28ee31e00680cd0140dbccb7aa
Cassiano <cassiano.costa@livetouch.com.br>
2020-06-21 18:39:08 -0300

    Detalhes character funcionando

--- app/src/main/java/com/example/rickandmorty/Adapter/CharacterAdapter.kt
+++ app/src/main/java/com/example/rickandmorty/Adapter/CharacterAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.rickandmorty.Adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.rickandmorty.Activity.CharacterDetailActivity
 import com.example.rickandmorty.R
 import com.example.rickandmorty.VO.Character.CharacterVO
 import kotlinx.android.synthetic.main.card_character.view.*
@@ -26,10 +29,16 @@ class CharacterAdapter(var characterList: List<CharacterVO>) : RecyclerView.Adap
 }
 
 class CharacterViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-    fun bindView(characterList: CharacterVO) {
-        itemView.tvStatus.text = characterList.status
-        itemView.tvName.text = characterList.name
+    fun bindView(character: CharacterVO) {
+        itemView.tvStatus.text = character.status
+        itemView.tvName.text = character.name
 
-        Glide.with(itemView.context).load(characterList.image).into(itemView.ivCardCharacter)
+        Glide.with(itemView.context).load(character.image).into(itemView.ivCardCharacter)
+
+        itemView.cardCharacter.setOnClickListener {
+            val intent = Intent(itemView.context, CharacterDetailActivity::class.java)
+            intent.putExtra("id", character.id)
+            startActivity(itemView.context, intent, null)
+        }
     }
 }
--- app/src/main/java/com/example/rickandmorty/Services/Endpoints.kt
+++ app/src/main/java/com/example/rickandmorty/Services/Endpoints.kt
@@ -1,11 +1,17 @@
 package com.example.rickandmorty.Services
 
+import com.example.rickandmorty.VO.Character.CharacterVO
 import com.example.rickandmorty.VO.Character.ResultCharacterVO
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Endpoints {
 
     @GET("character/")
     fun getCharacters() : Call<ResultCharacterVO>
+
+    @GET("character/{id}")
+    fun getCharacterDetail(@Path("id") id : Int?) : Call<CharacterVO>
 }
